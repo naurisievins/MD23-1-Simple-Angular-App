c1bd0fee45d984ad0c84ade5360b42de5c1f5f38
naurisievins <nauris.ievins@gmail.com>
2023-03-01 21:43:32 +0200

    Added curency pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CurrencyPipe } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [CurrencyPipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
